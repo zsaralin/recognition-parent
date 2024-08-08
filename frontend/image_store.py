7af685dd858a436f7ea126a3e9878191f2ba845e
zsaralin <zsaralin@gmail.com>
2024-08-08 13:04:47 -0400

    added min frames for preload images

--- frontend/image_store.py
+++ frontend/image_store.py
@@ -2,6 +2,7 @@ import os
 import cv2
 from logger_setup import logger
 
+
 class ImageStore:
     def __init__(self):
         self.preloaded_images = {}
@@ -14,7 +15,8 @@ class ImageStore:
                     image_path = os.path.join(root, file)
                     image = cv2.imread(image_path)
                     if image is not None:
-                        sub_images = self.split_into_sub_images(image, 100, 100)
+                        num_images = self.get_num_images_from_filename(file)
+                        sub_images = self.split_into_sub_images(image, 100, 100, num_images)
                         sub_images_with_reversed = sub_images + sub_images[::-1]
                         self.preloaded_images[image_path] = sub_images_with_reversed
                         print(f"Preloaded image: {image_path}")
@@ -23,7 +25,7 @@ class ImageStore:
         logger.info('Preload images completed')
         return self.preloaded_images
 
-    def split_into_sub_images(self, image, sub_width, sub_height):
+    def split_into_sub_images(self, image, sub_width, sub_height, num_images):
         sub_images = []
         height, width, _ = image.shape
         for y in range(0, height, sub_height):
@@ -31,10 +33,21 @@ class ImageStore:
                 sub_image = image[y:y + sub_height, x:x + sub_width]
                 if sub_image.shape[0] == sub_height and sub_image.shape[1] == sub_width:
                     sub_images.append(sub_image)
+                if len(sub_images) >= num_images:
+                    break
+            if len(sub_images) >= num_images:
+                break
+
         return sub_images
+
+    def get_num_images_from_filename(self, filename):
+        # Extract the number from the filename assuming the format is like "image_50.png"
+        num_images = min(5, int(filename.split('_')[-1].split('.')[0]))
+        return num_images
 
     def get_sub_images(self, image_path):
         return self.preloaded_images.get(image_path, [])
+
 
 # Create a global instance
 image_store = ImageStore()
